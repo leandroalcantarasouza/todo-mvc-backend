051aab112a4d7117e4dd260004d5f484c6ea2934
Leandro Souza <leandro.alcantara.souza@gmail.com>
2019-04-22 19:53:03 -0300

    Registering cors filter

--- src/main/kotlin/com/leandro/todo/todobackend/TodoBackendApplication.kt
+++ src/main/kotlin/com/leandro/todo/todobackend/TodoBackendApplication.kt
@@ -2,12 +2,33 @@ package com.leandro.todo.todobackend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.boot.web.servlet.filter.OrderedFilter
+import org.springframework.context.annotation.Bean
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
 
 @SpringBootApplication
 @EnableMongoRepositories
-class TodoMvcBackendApplication
+class TodoMvcBackendApplication {
+
+
+    @Bean
+    fun corsFilter(): FilterRegistrationBean<CorsFilter?> {
+        val source = UrlBasedCorsConfigurationSource()
+        val corsConfiguration = CorsConfiguration()
+        corsConfiguration.allowedOrigins = listOf("*")
+        corsConfiguration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
+        source.registerCorsConfiguration("/**", corsConfiguration)
+        val filterRegistrationBean = FilterRegistrationBean(CorsFilter(source))
+        filterRegistrationBean.order = OrderedFilter.REQUEST_WRAPPER_FILTER_MAX_ORDER
+        return filterRegistrationBean
+    }
+
+}
 
 fun main(args: Array<String>) {
-	runApplication<TodoMvcBackendApplication>(*args)
+    runApplication<TodoMvcBackendApplication>(*args)
 }
