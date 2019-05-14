181a488e416cac1b3df17ba90a7836f51952e10f
Leandro Souza <leandro.alcantara.souza@gmail.com>
2019-05-14 01:59:47 -0300

    Beginning swagger configuration

--- src/main/kotlin/com/leandro/todo/todobackend/TodoBackendApplication.kt
+++ src/main/kotlin/com/leandro/todo/todobackend/TodoBackendApplication.kt
@@ -9,11 +9,20 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import org.springframework.web.filter.CorsFilter
+import springfox.documentation.builders.ApiInfoBuilder
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiInfo
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+import java.time.LocalDate
 import javax.validation.Validation
 import javax.validation.Validator
 
 @SpringBootApplication
 @EnableMongoRepositories
+@EnableSwagger2
 class TodoMvcBackendApplication {
 
     @Bean
@@ -32,6 +41,27 @@ class TodoMvcBackendApplication {
     fun validator() : Validator? {
        val factory = Validation.buildDefaultValidatorFactory()
        return factory.validator
+    }
+
+    @Bean
+    fun petApi(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+            .host("localhost:8080")
+            .apiInfo(apiInfo())
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("com.leandro.todo.todobackend"))
+            .paths(PathSelectors.any())
+            .build()
+            .useDefaultResponseMessages(false)
+    }
+
+    private fun apiInfo(): ApiInfo {
+
+        return ApiInfoBuilder()
+            .title("Documentacao da api de caso do sgo v1")
+            .version("0.0.1-SNAPSHOT")
+            .description("Gerado em " + LocalDate.now())
+            .build()
     }
 
 }
